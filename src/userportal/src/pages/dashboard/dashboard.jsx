0fd50903963a0b1ce92a5c66ff29ca3341671175
Emrah Ozdemir <emrah.ozdemir@deop.ca>
2025-04-28 01:30:23 -0400

    added chat function

--- src/userportal/src/pages/dashboard/dashboard.jsx
+++ src/userportal/src/pages/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CopilotChat from '../../components/CopilotChat';
 
 const Dashboard = () => {
   return (
@@ -6,7 +7,7 @@ const Dashboard = () => {
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 className="h2">Dashboard</h1>
       </div>
-
+      <CopilotChat />
     </div>
   );
 };
